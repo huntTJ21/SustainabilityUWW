79ba6d35f8c81e3655f23f69c720207ea59cb5c1
Thomas Hunt <HuntTJ21@uww.edu>
2019-10-31 11:24:51 -0500

    Worked on implemeting the Excel Library. Finished skeletons for ExcelControl, Workbook, and Spreadsheet classes.

---
+++ ExcelLib/ExcelControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelLib
+{
+    public class ExcelControl
+    {
+        public Excel.Application App { get; }
+        public List<Workbook> Workbooks { get; }
+
+        public ExcelControl()
+        {
+            App = new Excel.Application();
+            Workbooks = new List<Workbook>();
+        }
+
+        ~ExcelControl()
+        {
+            // Make sure to quit the COM App or it will stay on in the background,
+            //  causing file read errors and memory leaks.
+            App.Quit();
+        }
+
+        // Child Object Constructors
+        public void addWorkbook(string fullPath)
+        {
+            // Make sure the file exists before moving forward
+            if (!File.Exists(fullPath)) { throw new FileNotFoundException(); }
+
+            // Create the COM Object, making sure that there are no errors reading the file
+            try
+            {
+                // Create the COM object and store it in a Wrapper Class
+                Excel.Workbook WBObj = App.Workbooks.Open(fullPath);
+                Workbook newBook = new Workbook(this, WBObj, fullPath);
+
+                // Add the Wraper Object to the list
+                Workbooks.Add(newBook);
+            }
+            catch(System.Runtime.InteropServices.COMException ex)
+            {
+                // Known Error Codes
+                int FileAlreadyOpen = -2146827284;
+
+                if(ex.ErrorCode == FileAlreadyOpen)
+                {
+                    throw new FileLoadException();
+                }
+            }
+        }
+        
+    }
+}
